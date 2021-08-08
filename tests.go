9441ed008024706f3fbd2224ddb9f923c3447e53
erikmichaelson <>
2021-08-08 18:03:04 +0000

    fixed all the syntax in test copies

--- tests.go
+++ tests.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"unittest"
+	"math"
+	"testing"
 )
 // TODO: translate this
-from lis import *
+//from lis import *
 
 //class TestHelpers(unittest.TestCase):
 func TestParser(t *testing.T) {
@@ -15,16 +16,15 @@ func TestParser(t *testing.T) {
 		input string
 		expects []string
 	}{
-		{"(define r 10)", {"define","r","10"}},
-		{"(* pi (* r r))", {"*","pi","(* r r)"}},
-		{"(* a (- b c)(+ d e (* f g)))", {"*", "a","(- b c)", "(+ d e (* f g))"}}
-	}
+		{"(define r 10)", []string{"define","r","10"}},
+		{"(* pi (* r r))", []string{"*","pi","(* r r)"}},
+		{"(* a (- b c)(+ d e (* f g)))", []string{"*", "a","(- b c)", "(+ d e (* f g))"}}}
 
 	for _, test := range tests {
-		testname := input
+		testname := test.input
 
 		t.Run(testname, func(t *testing.T) {
-			ans := parse_next_depth_expr(input)
+			ans := parse_next_depth_expr(test.input)
 			if ans != test.expects {
 				t.Errorf("got %s, want %s", ans, test.expects)
 			}
@@ -33,44 +33,48 @@ func TestParser(t *testing.T) {
 }
 
 
-func TestLiterals(t *testing.T):
-	t.Run(testname, func(t *testing.T) {
+func TestLiterals(t *testing.T) {
+	t.Run("Literal", func(t *testing.T) {
 				ans := run_program("12")
 				if ans != 12 {
-					t.Errorf("got %s, want %s", ans, test.expects)
+					t.Errorf("got %s, want %s", ans, 12)
 				}
 			})
-	
-func TestBasicLists(t *testing.T):
+}
+
+func TestBasicLists(t *testing.T) {
 	var tests = []struct {
 			input string
-			expects []string
+			expects float64
 		}{
-			{"(* 3 2 1 14)", 			84},
+			{"(* 3 2 1 14)",			84.0},
 			{"(sqrt (* 2 8))",			4.0},
-			{"(define r 10)\nr", 			10},
-			{"(define r 10)\n(* pi (* r r))", 	math.pi * 100},
-			{"(* 2 (- 3 4)(+ 9 -2 (* 5 1)))", 	-24}
-		}
+			{"(define r 10)\nr",			10.0},
+			{"(define r 10)\n(* pi (* r r))",	math.Pi * 100},
+			{"(* 2 (- 3 4)(+ 9 -2 (* 5 1)))",	-24.0}}
 
 		for _, test := range tests {
-			testname := input
+			testname := test.input
 
 			t.Run(testname, func(t *testing.T) {
-				ans := run_program(input)
+				ans := run_program(test.input)
 				if ans != test.expects {
 					t.Errorf("got %s, want %s", ans, test.expects)
 				}
 			})
 		}
+}
 
-func TestAdvanced(t *testing.T):
-	t.Run(testname, func(t *testing.T) {
+/*
+need to figure out how to store and represent a function stored in execution to test this
+TODO
+
+func TestAdvanced(t *testing.T) {
+	t.Run("Advanced", func(t *testing.T) {
 		ans := run_program("(if (> (val x) 0)\n(fn (+ (aref A i) (* 3 i))\n(quote (one two)))")
 		if ans != test.expects {
-			t.Errorf("got %s, want %s", ans, text.expects)
+			t.Errorf("got %s, want %s", ans, test.expects)
 		}
 	})
+}	*/
 
-if __name__ == '__main__':
-	unittest.main()
